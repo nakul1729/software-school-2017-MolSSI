61128b91b3f3844b5806cc40d11246c8bf02bc21
nakul1729 <>
2017-07-25 15:59:28 -0400

    test modified

--- tests/test_math.py
+++ tests/test_math.py
@@ -8,3 +8,10 @@ import pytest
 def test_add():
     assert ss.math.add(5, 2) == 7
     assert ss.math.add(2, 5) == 7
+
+
+def test_mult():
+    assert ss.math.mult(5, 2) == 10
+    assert ss.math.mult(2, 5) == 10
+
+
