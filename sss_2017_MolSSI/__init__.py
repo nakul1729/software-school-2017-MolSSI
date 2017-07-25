97426466fc2d48eade4a3450af2019a934d28a34
nakul1729 <>
2017-07-25 14:29:38 -0400

    first commit

---
+++ sss_2017_MolSSI/__init__.py
@@ -0,0 +1,8 @@
+"""
+This is the base file of the software-school-2017-MolSSI
+
+"""
+
+from . import math
+from .math import add
+from .math import mult
---
+++ sss_2017_MolSSI/math.py
@@ -0,0 +1,17 @@
+"""
+A small set of functions for doing math operations
+"""
+
+# Write a function named add that adds two values
+def add(a, b):
+     """
+     Function that adds two arguments
+     """
+     return a + b
+
+
+def mult(a, b):
+     """
+     Function that adds two arguments
+     """
+     return a * b
