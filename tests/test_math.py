7f02439cb52d397ecc636459831f1264c4e14fd2
nakul1729 <>
2017-07-26 10:06:46 -0400

    + comment

--- tests/test_math.py
+++ tests/test_math.py
@@ -19,3 +19,16 @@ def test_subs():
     assert ss.math.subs(10, 2) == 8
     assert ss.math.subs(2, 10) == -8
 
+
+
+"""
+testdata = [
+	(2, 5, 10),
+	
+
+]
+@pytest.mark.parameterize("a, b, expected",testdata)
+
+Test multiplications or operations here
+
+"""
